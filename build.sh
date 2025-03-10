cf7cf6d62bf9fd88653de6e6abe5cd787a41f144
the salytanic one~ <95072066+Sexynos990@users.noreply.github.com>
2025-03-10 17:28:47 +0200

    i swear im fucking blind

--- build.sh
+++ build.sh
@@ -85,8 +85,27 @@ echo "Preparing the build environment..."
 pushd $(dirname "$0") > /dev/null
 CORES=$(grep -c processor /proc/cpuinfo)
 
+# Define toolchain variables
 CLANG_DIR=$PWD/toolchain/neutron_18
 PATH=$CLANG_DIR/bin:$PATH
+
+# Check if toolchain exists
+if [ ! -f "$CLANG_DIR/bin/clang-18" ]; then
+    echo "-----------------------------------------------"
+    echo "Toolchain not found! Downloading..."
+    echo "-----------------------------------------------"
+    rm -rf $CLANG_DIR
+    mkdir -p $CLANG_DIR
+    pushd toolchain/neutron_18 > /dev/null
+    bash <(curl -s "https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman") -S=05012024
+    echo "-----------------------------------------------"
+    echo "Patching toolchain..."
+    echo "-----------------------------------------------"
+    bash <(curl -s "https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman") --patch=glibc
+    echo "-----------------------------------------------"
+    echo "Cleaning up..."
+    popd > /dev/null
+fi
 
 if [[ "$CCACHE_OPTION" == "y" ]]; then
     CCACHE=ccache
